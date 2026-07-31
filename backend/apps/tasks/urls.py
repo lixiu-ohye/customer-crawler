916639d4ebfedfc8ede1f377a2dd8d5e446923fd
lixiu-ohye <2207707856@qq.com>
2026-07-31 21:17:10 +0800

    Deploy frontend dist to GitHub Pages

--- backend/apps/tasks/urls.py
+++
@@ -1,9 +0,0 @@
-"""任务模块路由"""
-from django.urls import path
-
-from apps.tasks.views import TaskDetailView, TaskListView
-
-urlpatterns = [
-    path("", TaskListView.as_view()),
-    path("<int:pk>", TaskDetailView.as_view()),
-]
